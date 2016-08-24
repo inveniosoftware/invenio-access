8c5a161871f05a04887469e07ad1b3f6d916e1f4
Alizee Pace <alizee.pace@gmail.com>
2016-08-24 11:14:12 +0200

    docs: autodoc configuration and HTTPS fix
    * Replaces HTTP links with HTTPS where possible.
    
    Signed-off-by: Alizee Pace <alizee.pace@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -137,8 +137,8 @@ html_theme_options = {
     'github_banner': True,
     'show_powered_by': False,
     'extra_nav_links': {
-        'invenio-access@GitHub': 'http://github.com/inveniosoftware/invenio-access',
-        'invenio-access@PyPI': 'http://pypi.python.org/pypi/invenio-access/',
+        'invenio-access@GitHub': 'https://github.com/inveniosoftware/invenio-access',
+        'invenio-access@PyPI': 'https://pypi.python.org/pypi/invenio-access/',
     }
 }
 
@@ -329,3 +329,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# Autodoc configuraton.
+autoclass_content = 'both'
