9dddfd746653f73c9bf94fdeaed619ee455363eb
Jiri Kuncar <jiri.kuncar@cern.ch>
2016-09-21 13:23:22 +0200

    docs: ReadTheDocs configuration
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015 CERN.
+# Copyright (C) 2015, 2016 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -31,7 +31,7 @@ import sphinx.environment
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.4.2'
 
 # Do not warn on external images.
 suppress_warnings = ['image.nonlocal_uri']
