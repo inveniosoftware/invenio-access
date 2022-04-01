f84312b0505c2c90c664490ec79aab10f65f7fad
Nicola Tarocco <ntarocco@gmail.com>
2022-04-01 09:52:29 +0200

    global: support Flask v2.1

--- invenio_access/__init__.py
+++ invenio_access/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2015-2022 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -401,6 +401,22 @@ can retrieve them via:
 >>> sorted(app.extensions['invenio-access'].actions.keys())
 ['admin-access', 'superuser-access']
 """
+
+# Monkey patch Werkzeug 2.1
+# Flask-Login uses the safe_str_cmp method which has been removed in Werkzeug
+# 2.1. Flask-Login v0.6.0 (yet to be released at the time of writing) fixes the
+# issue. Once we depend on Flask-Login v0.6.0 as the minimal version in
+# Flask-Security-Invenio/Invenio-Accounts we can remove this patch again.
+try:
+    # Werkzeug <2.1
+    from werkzeug import security
+    security.safe_str_cmp
+except AttributeError:
+    # Werkzeug >=2.1
+    import hmac
+
+    from werkzeug import security
+    security.safe_str_cmp = hmac.compare_digest
 
 from flask_principal import ActionNeed
 
