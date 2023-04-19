5e018fa1e90ca8d9c754b69bdf6d3211b31ca612
Martin Lettry <martin.lettry@cern.ch>
2023-04-19 14:06:35 +0200

    model: Update role_id column  * fix role instantiation  * closes https://github.com/inveniosoftware/invenio-app-rdm/issues/2186
    Co-authored-by: jrcastro2 <jrcastro9515@gmail.com>

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2017-2018 CERN.
+# Copyright (C) 2017-2023 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -26,40 +26,37 @@ from invenio_access.permissions import any_user, authenticated_user
 def test_load_permissions_on_request_loaded(app):
     """Checks that the needs are loaded during request in the user Identity."""
     InvenioAccess(app)
-    with app.test_request_context():
-        with app.test_client() as client:
-            client.get("/")
-            assert g.identity.provides == {any_user}
+    with app.test_client() as client:
+        client.get("/")
+        assert g.identity.provides == {any_user}
 
 
 def test_load_permissions_on_identity_loaded(app):
     """Check that the needs are loaded properly in the user Identity."""
     InvenioAccess(app)
 
-    with app.test_request_context():
-        identity_changed.send(
-            current_app._get_current_object(), identity=AnonymousIdentity()
-        )
-        assert g.identity.provides == {any_user}
+    identity_changed.send(
+        current_app._get_current_object(), identity=AnonymousIdentity()
+    )
+    assert g.identity.provides == {any_user}
 
-    with app.test_request_context():
-        user = testutils.create_test_user("test@example.org")
-        login_user(user)
-        assert g.identity.provides == {any_user, authenticated_user, UserNeed(user.id)}
-        logout_user()
-        # FIXME: The user is still authenticatd when the identity loader
-        # is called during logout. We could filter on AnonymousIdentity, but
-        # This would be inconsistent as the UserNeed(user.id) is still there.
-        # This will pass even if it is unexpected:
-        # assert g.identity.provides == {
-        #     any_user, authenticated_user, UserNeed(user.id)
-        # }
-        # Forcing the identity to reload again cleans the mess. In practice
-        # this won't be needed as the identity is reloaded between requests.
-        identity_changed.send(
-            current_app._get_current_object(), identity=AnonymousIdentity()
-        )
-        assert g.identity.provides == {any_user}
+    user = testutils.create_test_user("test@example.org")
+    login_user(user)
+    assert g.identity.provides == {any_user, authenticated_user, UserNeed(user.id)}
+    logout_user()
+    # FIXME: The user is still authenticatd when the identity loader
+    # is called during logout. We could filter on AnonymousIdentity, but
+    # This would be inconsistent as the UserNeed(user.id) is still there.
+    # This will pass even if it is unexpected:
+    # assert g.identity.provides == {
+    #     any_user, authenticated_user, UserNeed(user.id)
+    # }
+    # Forcing the identity to reload again cleans the mess. In practice
+    # this won't be needed as the identity is reloaded between requests.
+    identity_changed.send(
+        current_app._get_current_object(), identity=AnonymousIdentity()
+    )
+    assert g.identity.provides == {any_user}
 
 
 def test_disabled_loader(app):
@@ -69,8 +66,7 @@ def test_disabled_loader(app):
 
     assert load_permissions_on_identity_loaded not in identity_loaded.receivers.values()
 
-    with app.test_request_context():
-        identity_changed.send(
-            current_app._get_current_object(), identity=AnonymousIdentity()
-        )
-        assert g.identity.provides == set()
+    identity_changed.send(
+        current_app._get_current_object(), identity=AnonymousIdentity()
+    )
+    assert g.identity.provides == set()
