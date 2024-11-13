76bc5789bf5de1782d8031b82da09b896ca7998e
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-11-13 08:54:59 +0100

    release: v3.0.0

--- invenio_access/__init__.py
+++ invenio_access/__init__.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2022 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -438,7 +439,7 @@ from .permissions import (
 )
 from .proxies import current_access
 
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 
 __all__ = (
     "__version__",
